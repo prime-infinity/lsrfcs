5592dca3511410bb94502175a94701b52afd67f7
prime-infinity <soe.a.prime@gmail.com>
2025-08-29 00:00:13 +0100

    pushed files skipped by agent

--- src/LaserFocus/obj/Debug/net9.0-windows/LaserFocus.AssemblyInfo.cs
+++ src/LaserFocus/obj/Debug/net9.0-windows/LaserFocus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaserFocus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317f23e711d82f11f27665bee8635c98065e4624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4b1d2ee0f7907df82b4be5d4bfd5540ff816d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaserFocus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaserFocus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
